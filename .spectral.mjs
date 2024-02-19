92e5a7061faaca57f2568889f7a9d0845ecf09a5
Szymon <szymon.morgala@smartbear.com>
2024-02-19 14:49:20 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzNDA4NA/spectral.js?branch_slug=main&token=bf420f1c-58ee-4008-b92d-63008018eb64";
+export default { extends: ruleset };
